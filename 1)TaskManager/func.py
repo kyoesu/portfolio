d8afccde76bb12aafe9a938b13f4d82f599f70f8
kyoesu <roma.hells@mail.ru>
2023-10-07 20:21:42 +0400

    sql reduced to normal appearance

--- 1)TaskManager/func.py
+++ 1)TaskManager/func.py
@@ -1,7 +1,25 @@
 import sqlite3
 
-connection = sqlite3.connect('folio.db')
-cursor = connection.cursor()
-cursor.execute('''select * from users''')
-connection.commit()
-connection.close()
+from secr import *
+
+
+#connection.commit()
+
+
+def login(name,passw):
+    table="test"
+    connection = sqlite3.connect('folio.db')
+    cursor = connection.cursor()
+    if "@"in name and (".ru" in name or ".com"in name or ".org" in name):
+        cursor.execute(f'''select id,name from {table} where email={name} and pass={passw}''')
+    else:
+         cursor.execute(f'''select id,name from {table} where name={name} and pass={passw}''')
+    res=cursor.fetchall()
+    connection.close()
+    if res!="":
+        nick=""
+        id_u=''
+        
+        return (True,id_u,nick)
+    else:
+        return(False,"0","0")
