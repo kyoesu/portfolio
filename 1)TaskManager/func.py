5d2215795e4edabef02b590e09fc69130d581a4c
kyoesu <roma.hells@mail.ru>
2023-10-08 19:53:15 +0400

    fixing sql login...

--- 1)TaskManager/func.py
+++ 1)TaskManager/func.py
@@ -11,15 +11,19 @@ def login(name,passw):
     connection = sqlite3.connect('folio.db')
     cursor = connection.cursor()
     if "@"in name and (".ru" in name or ".com"in name or ".org" in name):
-        cursor.execute(f'''select id,name from {table} where email={name} and pass={passw}''')
+        cursor.execute(f'''select id,username from {table} where email='{name}' and pass='{passw}';''')
     else:
-         cursor.execute(f'''select id,name from {table} where name={name} and pass={passw}''')
+         cursor.execute(f'''select id,username from {table} where username='{name}' and pass='{passw}';''')
     res=cursor.fetchall()
+    #print(res)
     connection.close()
-    if res!="":
-        nick=""
-        id_u=''
+    if res!=[]:
+        nick=res[0,1]
+        id_u=res[0,0]
         
         return (True,id_u,nick)
     else:
         return(False,"0","0")
+
+a,n,p=login ("hellmin","123456")
+print(f"проверка: {a}\n\r id= {n}; name= {p}")
