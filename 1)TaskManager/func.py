52889192d0626332c9ceb6c13865ad26ae4ea309
kyoesu <roma.hells@mail.ru>
2023-10-08 23:22:08 +0400

    add func sign up

--- 1)TaskManager/func.py
+++ 1)TaskManager/func.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from secr import *
+from secr import to_sim,to_let,dictionary
 
 
 #connection.commit()
@@ -11,19 +11,47 @@ def login(name,passw):
     connection = sqlite3.connect('folio.db')
     cursor = connection.cursor()
     if "@"in name and (".ru" in name or ".com"in name or ".org" in name):
-        cursor.execute(f'''select id,username from {table} where email='{name}' and pass='{passw}';''')
+        cursor.execute(f'''select id,username from {table} where email='{to_sim(name,dictionary)}' and pass='{to_sim(passw,dictionary)}';''')
     else:
-         cursor.execute(f'''select id,username from {table} where username='{name}' and pass='{passw}';''')
+         cursor.execute(f'''select id,username from {table} where username='{to_sim(name,dictionary)}' and pass='{to_sim(passw,dictionary)}';''')
     res=cursor.fetchall()
-    print(type(res[0]))
+    
     connection.close()
     if res!=[]:
         nick=res[0][1]
         id_u=res[0][0]
         
-        return (True,id_u,nick)
+        return (True,to_let(id_u,dictionary),to_let(nick,dictionary))
     else:
         return(False,"0","0")
 
-a,n,p=login ("hellmin","123456")
-print(f"проверка: {a}\n\r id= {n}; name= {p}")
+def sign_up_back(name,email,passw):
+    table="test"
+    connection = sqlite3.connect('folio.db')
+    cursor = connection.cursor()
+    cursor.execute(f'''select * from {table} where email='{email}' or username='{name}';''')
+    if (cursor.fetchall()==[]):
+        cursor.execute(f'''insert into {table} ([username],[email],[pass]) values ('{name}','{email}','{to_sim(passw,dictionary)}')''')
+        connection.commit()
+        connection.close()
+        return "ok"
+    else:
+        cursor.execute(f"select * from {table} where email='{email}'")
+        if (cursor.fetchall()!=[]):
+            connection.close()
+            return "email уже зарагастрирован"
+        cursor.execute(f"select * from {table} where username='{name}'")
+        if (cursor.fetchall()!=[]):
+            connection.close()
+            return "такой ник уже существует"
+        connection.close()
+        return("что-то пошло не так")
+    
+    
+
+
+'''
+a,n,p=login ("hellmin","123457")
+print(f"проверка: {a};\n\r id={n}; name={p}")'''
+
+print(sign_up_back("qqq","aa@aa.aa","qwerty"))
