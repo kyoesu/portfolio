6cdcdd8905e1c0ac5ef35f8ad63985cdc479c18e
kyoesu <roma.hells@mail.ru>
2023-10-16 23:37:45 +0400

    addin files to flask-login

---
+++ 1)TaskManager/UserLogin.py
@@ -0,0 +1,19 @@
+from flask_login import UserMixin
+
+class UserLogin(UserMixin):
+    def fromDB(self, user_id, db):
+        self.__user = db.getUser(user_id)
+        return self
+
+    def create(self, user):
+        self.__user = user
+        return self
+
+    def get_id(self):
+        return str(self.__user['id'])
+    
+    def is_authenticated(self):
+        return True
+    
+    def is_active(self):
+        return True
